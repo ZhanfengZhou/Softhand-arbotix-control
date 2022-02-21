89e920e3fa74ae43b07f1ad3e1bc9322c1e6ff66
ZhanfengZhou <zhanfeng.zhou@mail.utoronto.ca>
2022-02-20 23:07:34 -0500

    new grasp code

--- softhand_control_arbotix/softhand_control_arbotix/softhand_control_grasp.py
+++ softhand_control_arbotix/softhand_control_arbotix/softhand_control_grasp.py
@@ -24,41 +24,93 @@ class Softhand_Publisher(Node):
         self.publisher_array = [self.publisher1_, self.publisher2_, self.publisher3_, self.publisher4_,
                     self.publisher5_, self.publisher6_, self.publisher7_, self.publisher8_,
                     self.publisher9_, self.publisher10_]
-        timer_period = 10  # seconds
+        
+        self.declare_parameter('bend_angle_array_point0')    #in degrees
+        self.declare_parameter('bend_angle_array_point1')
+        self.declare_parameter('bend_angle_array_point2')
+        self.declare_parameter('wave_angle_array_point0')
+        self.declare_parameter('wave_angle_array_point1')
+        
+        self.publish_motors_angle_steps()
+        
+    def publish_motors_angle_steps(self):
+    
+        # get and process parameter:
+        self.bend_angle_array_point0 = self.get_parameter('bend_angle_array_point0').value
+        self.bend_angle_array_point1 = self.get_parameter('bend_angle_array_point1').value
+        self.bend_angle_array_point2 = self.get_parameter('bend_angle_array_point2').value
+        self.wave_angle_array_point0 = self.get_parameter('wave_angle_array_point0').value
+        self.wave_angle_array_point1 = self.get_parameter('wave_angle_array_point1').value
+        
+        self.bend_angle_array_r_point0 = self.process_angle_param(self.bend_angle_array_point0, 'bend')
+        self.bend_angle_array_r_point1 = self.process_angle_param(self.bend_angle_array_point1, 'bend')
+        self.bend_angle_array_r_point2 = self.process_angle_param(self.bend_angle_array_point2, 'bend')
+        self.wave_angle_array_r_point0 = self.process_angle_param(self.wave_angle_array_point0, 'wave')
+        self.wave_angle_array_r_point1 = self.process_angle_param(self.wave_angle_array_point1, 'wave')
+        
+        #step 1: 
+        self.get_logger().info('Moving soft hand! initializing ... \n ... ... \n ... ...')
+        time.sleep(2)  # wait to start, or the first step will not work!!
+        self.get_logger().info('Fingers moving to first point... ...')
+        self.publishing_bend_angle(self.bend_angle_array_r_point0)
+        #self.publishing_wave_angle(self.wave_angle_array_r_point0)
+        
+        time.sleep(32)
+        
+        #step 2:
+        self.get_logger().info('Fingers moving to second point... ...')
+        self.publishing_bend_angle(self.bend_angle_array_r_point1)
+        
+        time.sleep(36)
+        
+        #step 3:
+        self.get_logger().info('Fingers moving to second point... ...')
+        self.publishing_bend_angle(self.bend_angle_array_r_point2)
+        
+        #step 4:
+        timer_period = 36  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         
-        self.declare_parameter('bend_angle_array')    #in degrees
-        self.declare_parameter('wave_angle_array')
-
     def timer_callback(self):
-        bend_angle_array = self.get_parameter('bend_angle_array').value
-        bend_angle_array_new = [x-150 for x in bend_angle_array]    # bending starts from -150
-        wave_angle_array = self.get_parameter('wave_angle_array').value
-        bend_angle_array_r = [x/180*numpy.pi for x in bend_angle_array_new]    #change to radians
-        wave_angle_array_r = [x/180*numpy.pi for x in wave_angle_array]
+
+        self.publishing_bend_angle(self.bend_angle_array_r_point0)
+        self.get_logger().info('Fingers moving back to original point... ...')
+
         
+    def process_angle_param(self, angle_array, move_types):
+        #process angle array to radians
+        angle_array_r = None
+        if move_types == 'bend':
+            angle_array = [x-150 for x in angle_array]
+            angle_array_r = [x/180*numpy.pi for x in angle_array]
+        elif move_types == 'wave':
+            angle_array_r = [x/180*numpy.pi for x in angle_array]
+        else:
+            self.get_logger().error('process_angle_param(): Wrong argument input!')
+        return angle_array_r
+        
+    def publishing_bend_angle(self, bend_angle_array_r_point):
+                
         fingerbend_2motorID = [3, 4, 5, 6, 1]
-        fingerwave_2motorID = [2, 8, 7, 10, 9]
-        
         for i in range(5):
             msg = Float64()
-            msg.data = bend_angle_array_r[i]
+            msg.data = bend_angle_array_r_point[i]
             motorID = fingerbend_2motorID[i]
             topic_name = 'dynamixel%s/command' % str(motorID)
             self.publisher_array[motorID-1].publish(msg)
-            self.get_logger().info('Publishing to {}: {}'.format(topic_name, 
-                str(bend_angle_array[i]) ))
+            self.get_logger().info('Publishing to {}: {}'.format(topic_name, str(msg.data) ))
+            
+    def publishing_wave_angle(self, wave_angle_array_r_point):
         
-        time.sleep(1)
-        
+        fingerwave_2motorID = [2, 8, 7, 10, 9]
         for i in range(5):
             msg = Float64()
-            msg.data = wave_angle_array_r[i]
+            msg.data = wave_angle_array_r_point[i]
             motorID = fingerwave_2motorID[i]
             topic_name = 'dynamixel%s/command' % str(motorID)
             self.publisher_array[motorID-1].publish(msg)   
-            self.get_logger().info('Publishing to {}: {}'.format(topic_name, 
-                str(wave_angle_array[i]) ))
+            self.get_logger().info('Publishing to {}: {}'.format(topic_name, str(msg.data) ))
+
         
 def main(args=None):
     rclpy.init(args=args)
